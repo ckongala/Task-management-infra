5c59c9bb789d39a4cf907c9b9644bcf833c14151
chinni <chinnikrishna2023@gmail.com>
2024-11-26 19:40:06 -0500

    changes

--- eks-cluster/vpc.tf
+++ eks-cluster/vpc.tf
@@ -4,11 +4,9 @@ module "my-vpc" {
   version              = "5.8.1"
   name                 = "my-vpc"
   cidr                 = var.vpc_cidr_block
-  private_subnets      = var.private_subnet_cidr_blocks
   public_subnets       = var.public_subnet_cidr_blocks
   azs                  = data.aws_availability_zones.azs.names
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
   enable_dns_hostnames = true
   tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
@@ -16,9 +14,5 @@ module "my-vpc" {
   public_subnet_tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
     "kubernetes.io/role/elb"                  = 1
-  }
-  private_subnet_tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"         = 1
   }
 }
